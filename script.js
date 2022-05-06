55e51f40e796dc542904a532e1f2a59f70f96ddb
Matheus <matheuspl92@hotmail.com>
2022-05-06 17:12:58 -0300

    Add top button creation function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 init();
 
 function init(){
-    console.log('LOG: Page initialization')
+    createTopButton();
+    console.log('LOG: Page initialization complete')
+}
+
+function createTopButton(){
+    let gridButton = document.createElement('button');
+    gridButton.innerHTML = 'Change grid size';
+    gridButton.onclick = () => console.log('LOG: Grid button clicked!');
+    document.body.appendChild(gridButton);
 }
